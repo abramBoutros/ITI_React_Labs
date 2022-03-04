96dcd8d4b7f7773c646ef61dbea5f079675a5233
Abram Boutros <abramboutros96@gmail.com>
2022-03-04 21:31:46 +0200

    day2 react task

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import Login from "./Login";
+import Register from "./Register";
+
+function App() {
+	return (
+		<div className="container">
+			<Login />
+			<br />
+			<hr />
+			<br />
+			<hr />
+			<Register />
+		</div>
+	);
+}
+
+export default App;
